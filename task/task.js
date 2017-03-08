f2de0eaa401498d11ebdf54d2496e1814e550ec8
Yifei Wang <hello.yifei@icloud.com>
2017-03-08 21:15:40 +0100

    modified task

--- task/task.js
+++ task/task.js
@@ -5,55 +5,44 @@ window.onload = function(){
   let view = document.getElementById("view");
   let inputTitle = document.getElementById("inputEmail3");
   let inputAuthor = document.getElementById("inputAuthor");
+  function getError(message){
+    return `<div class="alert alert-danger" role="alert">
+    <strong>Oh snap!</strong> System error: ${message}.
+    </div>`
+  }
   add.addEventListener("click", function(event) {
-   fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=M7y37&title=${inputTitle}&author=${inputAuthor}`)
-     .then(function(res) {
-       if (res.status !== 200) {
-         addBook.innerHTML = 'Looks like there was a problem. Status Code: ' +
-           res.status;
-         return;
-       } //if
-       res.json().then(function(data) {
-         if(data.status==="error"){
-           addBook.innerHTML = `status: ${data.status}`;
-         }
-         else{
-        //    addBook.innerHTML = `bookName: ${inputTitle.value},
-        //    bookId: ${data.id}`;
-         addBook.innerHTML="your book have been added";
-        }
 
-       }); //res.json
-       // .catch(function(err) {
-       //
-       //   console.log(err);
-       // });
-     })//then func1
-} )
-viewButton.addEventListener("click", function(event) {
- fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=M7y37&title=${inputTitle}&author=${inputAuthor}`)
-   .then(function(res) {
-     if (res.status !== 200) {
-       addBook.innerHTML = 'Looks like there was a problem. Status Code: ' +
-         res.status;
-       return;
-     } //if
-     res.json().then(function(data) {
-         if(data.status==="error"){
-           addBook.innerHTML = `status: ${data.status}`;
-         }
-         else{
-          let listItems = document.createElement("p");
-          listItems.innerHTML = `bookName: ${inputTitle.value}, bookId: ${data.id}`;
-          addBook.appendChild(listItems);
-        }
+    fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=M7y37&title=${inputTitle.value}&author=${inputAuthor.value}`)
+    .then(function(res) {
+      return res.json()
+    })
+    .then(function(data) {
+      if(data.status==="error") throw "Failed to add book";
+      addBook.innerHTML="your book have been added";
+    })
+    .catch(function(error){
+       addBook.innerHTML = getError(error);
+    });
 
-     }); //res.json
-     // .catch(function(err) {
-     //
-     //   console.log(err);
-     // });
-   })//then func1
-})
+  })
 
-} //window load
+  viewButton.addEventListener("click", function(event) {
+    fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=M7y37`)
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      if(data.status === "error") throw data.message;
+
+      var books = data.data;
+      for(let i=books.length-1;i>=400;i--){
+        let listItems = document.createElement("p");
+        listItems.innerHTML = `bookTitle: ${books[i].title}, bookAuthor: ${books[i].author}, bookId: ${books[i].id}`;
+        addBook.appendChild(listItems);
+      }
+    })
+    .catch(function(error) {
+      addBook.innerHTML = getError(error);
+    });
+  });
+}
